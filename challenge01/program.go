d867568871bbafbbfae44270f35a1099e4a870e8
Anthony Luc <anthonyvanluc@gmail.com>
2020-01-30 14:20:49 -0500

    init program.go

---
+++ challenge01/program.go
@@ -0,0 +1,17 @@
+/**
+ * Challenge 01
+ *
+ * https://www3.nd.edu/~pbui/teaching/cse.30872.fa19/challenge01.html
+ *
+ * ## My Thoughts
+ *
+ * ## Time Complexity
+ *
+ * ## Space Complexity
+ *
+ */
+
+ package main
+
+ func main() {
+ }
