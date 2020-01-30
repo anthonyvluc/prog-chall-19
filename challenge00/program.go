f0c929774a791b4c4bda40be93ad77881c72b6ea
Anthony Luc <anthonyvanluc@gmail.com>
2020-01-29 23:59:25 -0500

    solved??

--- challenge00/program.go
+++ challenge00/program.go
@@ -16,30 +16,63 @@ import (
     "bufio"
     "fmt"
     "log"
+    "math"
     "os"
     "strconv"
 )
 
 func main() {
-    var n_numbers int
-    var err error
+    var n int          = 0
+    var deck int       = 1
+    var err error      = nil
+    var s string       = ""
+
+    printResults := func(s string) {
+        var index int      = 0
+        var sub_int int    = 0
+        var prev_char rune = 0
+        var result string  = ""
+
+        for _, char := range(s) {
+            if char >= prev_char {
+                index += 1
+            } else {
+                break
+            }
+            prev_char = char
+        }
+
+        if (index == 1 && len(s) == 1) || index == len(s) {
+            result = s
+        } else {
+            sub_int, err = strconv.Atoi(s[:index])
+            if err != nil {
+                log.Println(err)
+            }
+            result = strconv.FormatInt(int64(sub_int*int(math.Pow10(len(s)-index)) - 1), 10)
+        }
+
+        fmt.Println(fmt.Sprintf("Deck #%d: %s", deck, result))
+        deck += 1
+    }
 
     /* Read from stdin. */
     scanner := bufio.NewScanner(os.Stdin)
 
     /* Read n_numbers. */
     scanner.Scan()
-    n_numbers, err = strconv.Atoi(scanner.Text())
+    n, err = strconv.Atoi(scanner.Text())
     if err != nil {
         log.Println(err)
     }
+    n += 1
 
     /* Read and evaluate the rest of the numbers. */
     for scanner.Scan() {
-        fmt.Println(scanner.Text())
+        s = scanner.Text()
+        printResults(s)
     }
     if err := scanner.Err(); err != nil {
         log.Println(err)
     }
 }
-
