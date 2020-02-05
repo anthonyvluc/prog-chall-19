6c26c7eaebd587e578ba049511a8f25e54d92d34
Anthony Luc <anthonyvanluc@gmail.com>
2020-02-05 14:11:30 -0500

    init program

---
+++ challenge02/program.go
@@ -0,0 +1,26 @@
+/*
+ * # Challenge02
+ *
+ * https://www3.nd.edu/~pbui/teaching/cse.30872.fa19/challenge02.html
+ *
+ * ## My Idea
+ *
+ * ## Time Complexity
+ *
+ * ## Space Complexity
+ *
+ */
+
+package main
+
+import (
+    "fmt"
+)
+
+func challenge02() {
+    fmt.Println("Hi")
+}
+
+func main() {
+    challenge02()
+}
