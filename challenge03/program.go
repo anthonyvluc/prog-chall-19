46b43acb8e85888093a4583accb7de7aefb69029
Anthony Luc <anthonyvanluc@gmail.com>
2020-02-10 14:53:48 -0500

    init challenge03 with hello world

---
+++ challenge03/program.go
@@ -0,0 +1,22 @@
+/**
+ * # Challenge03
+ *
+ * https://www3.nd.edu/~pbui/teaching/cse.30872.fa19/challenge03.html
+ *
+ * ## My Idea
+ *
+ * ## Time Complexity
+ *
+ * ## Space Complexity
+ *
+ */
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Hello World")
+}
